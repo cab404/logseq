106578f4bf0be109c804a925eff6b45b7cc67d09
Vladimir Serov <me@cab404.ru>
2022-02-05 05:23:07 +0300

    trying to get part of flake working

---
+++ default.nix
@@ -0,0 +1,54 @@
+{ pkgs, cacert, mkYarnPackage, electron_15, clojure, curl, nodejs, openjdk11, makeWrapper, git }: let
+    clojureDeps = pkgs.stdenvNoCC.mkDerivation {
+        name = "deps";
+        src = ./deps.edn;
+        nativeBuildInputs = [ clojure git cacert ];
+        outputHashAlgo = null;
+        outputHashMode = "recursive";
+        hash = "sha256-PzBr8C5OmsCk28bnvUe0xFO9iKdZCtip7TrqETHM+UA=";
+        preferLocalBuild = false;
+        unpackPhase = ''
+            cp $src deps.edn
+        '';
+        buildPhase = ''
+            mkdir build
+            mv deps.edn build
+            cd build
+            export HOME=$PWD
+            clojure -P
+        '';
+        installPhase = ''
+            cd ..
+            mv build $out
+        '';
+    };
+    # classp  = clojureDeps.makeClasspaths {};
+in clojureDeps
+# mkYarnPackage rec {
+#     pname = "logseq";
+#     src = ./.;
+
+#     yarnPreBuild = ''
+#         mkdir -p $TMPDIR/home
+#         export ELECTRON_SKIP_BINARY_DOWNLOAD=1
+#     '';
+#     buildPhase = ''
+#         export HOME=$TMPDIR/home
+#         echo ${clojureDeps}
+
+#         # requires nodejs
+#         clojure -M:cljs release worker-parser app electron
+#     '';
+
+#     yarnFlags = [ "--offline" "--production" ];
+
+#     nativeBuildInputs = [ makeWrapper clojure git openjdk11 nodejs ];
+
+#     distPhase = ":"; # disable useless $out/tarballs directory
+
+#     postInstall = ''
+#         makeWrapper ${electron_15}/bin/electron $out/bin/logseq \
+#         --set NODE_ENV production \
+#         --add-flags $dir/build/main/main.js
+#     '';
+# }
---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    # clj2nix.url = "github:hlolli/clj2nix";
+  };
+
+  outputs = { nixpkgs, self, utils }: utils.lib.eachDefaultSystem (system:
+    let
+    pkgs = import nixpkgs { inherit system; };
+    # clj2nixBin = clj2nix.defaultPackage.${system};
+    in
+    {
+      defaultPackage = pkgs.callPackage ./. { };
+    });
+
+}
